837b6343a8832ae9717e54bd516c792bb2b95bf8
Vivek <vivek.bhageria@gmail.com>
2022-03-05 17:08:38 +0530

    implemented Denbu_Mem_Dump and basic functionality of Denbu_Mem_Alloc

--- denbu_mem.c
+++ denbu_mem.c
@@ -7,6 +7,18 @@
 #include <fcntl.h>
 #include "denbu_mem.h"
 
+
+
+
+typedef struct __ourList_t{
+    int size;
+    struct __ourList_t *next;
+} ourList_t; // our list structure
+
+//head of list
+ourList_t *head = NULL;
+
+
 int m_error;
 // number of calls made to mem_init
 int callsToInit = 0;
@@ -40,6 +52,10 @@ int Denbu_Mem_Init(int sizeOfRegion)
         perror("mmap");
         exit(1);
     }
+    head = memmptr;
+    head->size = sizeOfRegion;
+    head->next = NULL;
+    
     // this will not free the memory we got from OS.
     close(fd);
     
@@ -53,4 +69,65 @@ int Denbu_Mem_Init(int sizeOfRegion)
 // 
 // TODO: Library should fill the rest of the functions
 // Denbu_Mem_Alloc, Denbu_Mem_Free, Denbu_Mem_Dump
-//
+//Vivek's implementations below
+
+
+typedef struct __header_t{
+    int size;
+}header_t; //header structure for each block of allocated memory
+
+
+void *Denbu_Mem_Alloc(int size, int style)
+{
+    
+    if(size%8 != 0)  //  memory to be allocated in 8 byte chunks
+    {
+        size = size + (8 - size%8);
+    }
+
+    header_t *hPointer = NULL; //initialize head pointer
+    int newSize =0;
+    void *memPointer = NULL;
+    ourList_t *tmp = head;
+
+    while(tmp)
+    {
+        if (tmp->size >= size + sizeof(header_t))
+        {
+            memPointer = tmp;
+            break;
+        }
+        tmp= tmp->next;
+    }
+        
+
+    if(memPointer != NULL)
+    {
+        tmp = (void *) memPointer;
+        ourList_t *tmpNext = tmp->next; //place holder for next
+        newSize = tmp->size - size - sizeof(header_t);
+        memPointer = memPointer + tmp->size - size;
+        hPointer = (void *)memPointer - sizeof(header_t);
+        hPointer->size = size;
+        tmp->size = newSize;
+        tmp->next = tmpNext; //when you update size next changes so set it back
+    }
+    else if (memPointer == NULL)
+    {
+        m_error = E_NO_SPACE;
+        return NULL;
+    }
+    printf("returned ptr from alloc: %p\n", memPointer);
+    return (void *)memPointer;
+}
+
+
+void Denbu_Mem_Dump()
+{
+    printf("Printing free regions\n");
+    ourList_t *tmp = head;
+    while (tmp) {
+        printf("Free Size: %d\tPointer: %p\n",tmp->size, tmp);
+        tmp = tmp->next;
+    }
+}
--- main.c
+++ main.c
@@ -9,6 +9,8 @@ int main(int argc, const char *argv[]){
 
 	int size = 65536;
     assert( Denbu_Mem_Init(size) == 0);
+    printf("Alloc memory = %d\n",Denbu_Mem_Alloc(4096,1));
+    Denbu_Mem_Dump();
 
 	exit(1); 
 }
